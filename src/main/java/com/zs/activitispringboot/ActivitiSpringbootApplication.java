479d1d3001b66d712ef5a55065841b6f25445e72
liuyibo666666 <754869828@qq.com>
2018-11-09 20:45:53 +0800

    Initial commit

---
+++ src/main/java/com/zs/activitispringboot/ActivitiSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.zs.activitispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiSpringbootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zs/activitispringboot/ActivitiSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zs.activitispringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivitiSpringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
